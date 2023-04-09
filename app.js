1c4547d8cce2c5501ded6a4e2b29b1540e619478
Abhishek <kunal613.kv@gmail.com>
2023-04-09 15:25:32 +0530

    Authentication and Authorization added for the sites and the reviews

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
-
-const campgrounds = require("./routes/campgrounds")
-const reviews = require("./routes/reviews")
+const userRoutes = require("./routes/users")
+const campgroundRoutes = require("./routes/campgrounds")
+const reviewRoutes = require("./routes/reviews")
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
@@ -31,6 +34,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, "public")))
 
+
 const sessionConfig = {
   secret: 'thishouldbeabettersecret!',
   resave: false,
@@ -45,14 +49,25 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
+  // console.log(req.session)
+  res.locals.currentUser = req.user;
   res.locals.success = req.flash('success')
   res.locals.error = req.flash("error")
   next();
 })
 
-app.use("/campgrounds",campgrounds)
-app.use("/campgrounds/:id/reviews", reviews)
+
+app.use("/",userRoutes)
+app.use("/campgrounds",campgroundRoutes)
+app.use("/campgrounds/:id/reviews", reviewRoutes)
 
 
 app.get("/", (req, res) => {
@@ -67,7 +82,7 @@ app.use((err,req,res,next)=>{
   const {statusCode = 500}=err;
   if(!err.message) err.message = "Ohh No, Something went wrong"
   res.status(statusCode).render("error",{err})
-})
+});
 
 app.listen(3000, () => {
   console.log("listening on 3000");
--- routes/reviews.js
+++ routes/reviews.js
@@ -5,23 +5,15 @@ const Review = require("../models/review");
 const Campground = require("../models/campground");
 const catchAsync = require("../utils/catchAsync");
 const ExpressError = require("../utils/ExpressError");
-
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map(el => el.message).join(", ");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
+const {validateReview,isLoggedIn} = require("../middleware.js")
 
 router.post(
-  "/",
+  "/",isLoggedIn,validateReview,
   catchAsync(async (req, res) => {
     console.log("wer");
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     console.log("we");
     await review.save();
